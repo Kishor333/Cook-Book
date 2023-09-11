6d64d8521c138e2f0cd66ebab55312073db65792
Kishor Kumar Mongar <kishorkumarmongar@gmail.com>
2023-09-11 12:11:39 +0600

    page not found added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  { path:'', component:RecipesComponent },
-  // { path:'recipe', component:RecipesComponent },
+  { path: '', redirectTo: 'recipe', pathMatch: 'full' },
+  { path:'recipe', component:RecipesComponent , children:[
+    {path:':id', component: RecipeDetailComponent}
+  ] },
+  
   { path:'shoppingList', component:ShoppingListComponent },
+
+  { path:'not-found', component:NotFoundComponent },
+  { path:'**', redirectTo: '/not-found', pathMatch:'full' },
 ]; 
 
 @NgModule({
