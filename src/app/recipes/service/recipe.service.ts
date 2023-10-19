56030e528d8945219463eedba73c7e216e62ea3f
Kishor Kumar Mongar <kishorkumarmongar@gmail.com>
2023-10-19 16:38:04 +0600

    fixed the image import

--- src/app/recipes/service/recipe.service.ts
+++ src/app/recipes/service/recipe.service.ts
@@ -21,7 +21,7 @@ export class RecipeService {
   // private recipy:Recipe[] =[
   //     { name:'Chorizo & mozzarella gnocchi bake',
   //       description: 'Upgrade cheesy tomato pasta with gnocchi, chorizo and mozzarella for a comforting bake that makes an excellent midweek meal',
-  //       imgPath:'rec1.webp',
+  //       imgPath:'./assets/images/rec1.webp',
   //       ingredient: [
   //         {name:'Chees', amount:1},
   //         {name:'Pasta', amount:2},
@@ -29,7 +29,7 @@ export class RecipeService {
   //     },
   //     {name:'Chocolate fudge cake',
   //      description: 'Need a guaranteed crowd-pleasing cake thats easy to make? This super-squidgy chocolate fudge cake with smooth icing is an instant baking win', 
-  //      imgPath:'choco.webp',
+  //      imgPath:'./assets/images/choco.webp',
   //     ingredient:[
   //         {name:'Chocolate', amount:5},
   //         {name:'Flour', amount:2},
@@ -39,7 +39,7 @@ export class RecipeService {
   //   },
   //     {name:'Fresh salmon niçoise',
   //      description: 'On a balmy summer evening, a warm salad is satisfying yet light. Traditionally, niçoise is made with canned tuna, but the omega-3 fatty acids in the fish don’t make it through the canning process, so we’ve used wild salmon instead',
-  //     imgPath:'salmon.webp',
+  //     imgPath:'./assets/images/salmon.webp',
   //     ingredient:[
   //       {name:'salad', amount:3},
   //       {name:'yuna', amount:1},
@@ -47,7 +47,7 @@ export class RecipeService {
   //   },
   //     {name:'Hummus without tahini',
   //      description: 'Enjoy our easy, budget-friendly hummus recipe with your favourite sides, such as pittas, crackers and crudités. Add more liquid from the can of chickpeas for a smoother hummus, if you like.',
-  //       imgPath:'humus.webp',
+  //       imgPath:'./assets/images/humus.webp',
   //     ingredient:[
   //       {name:'Pittas', amount:3},
   //       {name:'cracker', amount:1},
@@ -56,7 +56,7 @@ export class RecipeService {
   //   },
   //     {name:'Potato hash',
   //      description: 'Achieve three of your five-a-day with our potato hash. Peppers are packed with vitamin C and spinach is rich in iron – a great combo with hearty potatoes',
-  //       imgPath:'potato.webp',
+  //       imgPath:'./assets/images/potato.webp',
   //     ingredient:[
   //       {name:'Potato', amount:3},
   //       {name:'Spinach', amount:2},
@@ -64,7 +64,7 @@ export class RecipeService {
   //   },
   //     {name:'Gluten-free flatbread',
   //      description: 'Use our easy gluten-free bread recipe to make light, fluffy breads which can be used for scooping up sauces, served alongside curries or served as a lunchtime snack with some dips',
-  //       imgPath:'flatbread.webp',
+  //       imgPath:'./assets/images/flatbread.webp',
   //     ingredient:[
   //       {name:'Flour', amount:1},
   //         {name:'Sauce', amount:2},
@@ -72,7 +72,7 @@ export class RecipeService {
   //   },
   //     {name:'Chicken koftas',
   //      description: 'Enjoy chicken koftas with hummus and salad for a healthy lunch. The tahini in the hummus is made from sesame seeds, which are thought to have anti-inflammatory properties',
-  //       imgPath:'chickenkuffat.webp',
+  //       imgPath:'./assets/images/chickenkuffat.webp',
   //     ingredient:[
   //       {name:'Chicken(mother)', amount:1},
   //       {name:'Salad', amount:2},
